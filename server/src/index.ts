0293d5672c6356112f625c9b9ccfe4aa5899e618
Rasluka <vargklee@hotmail.com>
2025-02-14 10:39:38 -0600

    Adding PostgreSQL and dotenv packages

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,13 +16,13 @@ app.listen(PORT, () => {
 
 app.post("/users", (req: Request, res: Response) => {
   try {
-    const { nickname } = req.body;
+    const { nickname, email } = req.body;
 
-    if (!nickname) {
-      throw new Error("Nickname is missing in body request!");
+    if (!nickname || !email) {
+      throw new Error("Nickname and email are required!");
     }
 
-    const newCode = nanoid(8);
+    const newCode = nanoid(8).toUpperCase();
 
     console.log(`Creating a new code for ${nickname}::: ${newCode}`);
 
@@ -32,7 +32,6 @@ app.post("/users", (req: Request, res: Response) => {
       message: "User created succesfully!",
     });
   } catch (error) {
-    // console.error("Error:", error instanceof Error ? error.message : error);
     res.status(400).json({
       error: error instanceof Error ? error.message : "Unknown error",
     });
