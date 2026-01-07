1648f3809f6b89e61103beca0c88f1add046971a
Rasluka <vargklee@hotmail.com>
2026-01-06 20:25:57 -0600

    chore:Created mockSignUp and updated SignUp UI

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,12 +1,14 @@
-import type { JSX } from "react";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { signUp } from "../services/authService";
-import { isValidEmail } from "../utils/validators";
-import toast, { Toaster } from "react-hot-toast";
-import { CodeModal } from "../components/CodeModal";
-import { useNavigate } from "react-router-dom";
-import { AxiosError } from "axios";
+import type { JSX } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+// import { signUp } from "../services/authService";
+import { isValidEmail } from '../utils/validators';
+import toast from 'react-hot-toast';
+import { CodeModal } from '../components/CodeModal';
+import { useNavigate } from 'react-router-dom';
+import { AxiosError } from 'axios';
+import { mockSignUp as signUp } from '../services/mockAutService';
+import { ShieldUser, Mail } from 'lucide-react';
 
 interface ISignUpStateForm {
   nickname: string;
@@ -20,12 +22,12 @@ interface INewUser {
 
 export default function SignUp(): JSX.Element {
   const [formData, setFormData] = useState<ISignUpStateForm>({
-    nickname: "",
-    email: "",
+    nickname: 'test',
+    email: 'test@test.com',
   });
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isNicknameError, setIsNicknameError] = useState<boolean>(false);
-  const [isEmailError, setIsEmailError] = useState<boolean>(false);
+  // const [isNicknameError, setIsNicknameError] = useState<boolean>(false);
+  // const [isEmailError, setIsEmailError] = useState<boolean>(false);
   const [showModal, setShowModal] = useState<boolean>(false);
   const [newUserData, setNewUserData] = useState<INewUser | null>(null);
   const navigate = useNavigate();
@@ -41,11 +43,11 @@ export default function SignUp(): JSX.Element {
 
   const onSubmitClicked = async () => {
     setIsLoading(true);
-    setIsNicknameError(false);
-    setIsEmailError(false);
+    // setIsNicknameError(false);
+    // setIsEmailError(false);
 
     if (!isValidEmail(formData.email)) {
-      setIsEmailError(true);
+      // setIsEmailError(true);
       setIsLoading(false);
       return;
     }
@@ -60,52 +62,52 @@ export default function SignUp(): JSX.Element {
           code: code,
         });
       } else {
-        throw new Error("Something went wrong!!");
+        throw new Error('Something went wrong!!');
       }
     } catch (err: unknown) {
       // Use unknown instead of any
       // Check if it's an AxiosError
       if (err instanceof AxiosError) {
         const res = err.response;
-        console.error("error ===>", res);
-        const errMsg = res?.data?.message || err.message;
+        console.error('error ===>', res);
+        // const errMsg = res?.data?.message || err.message;
 
-        toast.error(errMsg, {
-          style: {
-            borderRadius: "10px",
-            background: "#333",
-            color: "#fff",
-          },
-        });
+        // toast.error(errMsg, {
+        //   style: {
+        //     borderRadius: '10px',
+        //     background: '#333',
+        //     color: '#fff',
+        //   },
+        // });
 
-        if (res) {
-          const { status, data } = res;
-          const message = (data.message || "").toLowerCase();
+        // if (res) {
+        //   const { status, data } = res;
+        //   const message = (data.message || '').toLowerCase();
 
-          if (status === 409) {
-            if (message.includes("email")) {
-              setIsEmailError(true);
-            } else if (message.includes("nickname")) {
-              setIsNicknameError(true);
-            }
-          }
-        }
+        //   if (status === 409) {
+        //     if (message.includes('email')) {
+        //       setIsEmailError(true);
+        //     } else if (message.includes('nickname')) {
+        //       setIsNicknameError(true);
+        //     }
+        //   }
+        // }
       } else if (err instanceof Error) {
         // Handle non-Axios errors
         toast.error(err.message, {
           style: {
-            borderRadius: "10px",
-            background: "#333",
-            color: "#fff",
+            borderRadius: '10px',
+            background: '#333',
+            color: '#fff',
           },
         });
       } else {
         // Handle unknown error types
-        toast.error("An unknown error occurred", {
+        toast.error('An unknown error occurred', {
           style: {
-            borderRadius: "10px",
-            background: "#333",
-            color: "#fff",
+            borderRadius: '10px',
+            background: '#333',
+            color: '#fff',
           },
         });
       }
@@ -116,66 +118,84 @@ export default function SignUp(): JSX.Element {
 
   const onModalClose = () => {
     setShowModal(false);
-    void navigate("/signin");
+    void navigate('/signin');
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <legend className="fieldset-legend text-2xl">Sign Up</legend>
+    <div className="flex items-center justify-center min-h-[calc(100vh-4rem)]">
+      <div className="card p-12 bg-base-100 rounded-box border border-base-300 shadow-lg">
+        <h1 className="text-2xl font-bold text-center mb-2">Sign Up</h1>
 
-        <label className="label text-secondary">Nickname</label>
-        <input
-          type="text"
-          className={`input ${isNicknameError && "input-error"}`}
-          placeholder="Nickname"
-          name="nickname"
-          value={formData.nickname}
-          onChange={onInputChange}
-          onFocus={() => setIsNicknameError(false)}
-        />
-        {isNicknameError && (
+        <fieldset className="my-2">
+          <label className="label font-bold">Nickname</label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <ShieldUser />
+            </div>
+            <input
+              type="text"
+              // className={`input ${isNicknameError && 'input-error'}`}
+              className="input-base"
+              placeholder="Nickname"
+              name="nickname"
+              value={formData.nickname}
+              onChange={onInputChange}
+              // onFocus={() => setIsNicknameError(false)}
+            />
+            {/* {isNicknameError && (
           <label className="label text-red-600">
             Nickname is already being used
           </label>
-        )}
+        )} */}
+          </div>
+        </fieldset>
 
-        <label className="label text-secondary">Email</label>
-        <input
-          type="email"
-          className={`input ${isEmailError && "input-error"}`}
-          placeholder="Email"
-          name="email"
-          value={formData.email}
-          onChange={onInputChange}
-          onFocus={() => setIsEmailError(false)}
-        />
-        {isEmailError && (
+        <fieldset className="my-2">
+          <label className="label font-bold">Email</label>
+
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <Mail />
+            </div>
+
+            <input
+              type="email"
+              // className={`input ${isEmailError && 'input-error'}`}
+              className="input-base"
+              placeholder="Email"
+              name="email"
+              value={formData.email}
+              onChange={onInputChange}
+              // onFocus={() => setIsEmailError(false)}
+            />
+            {/* {isEmailError && (
           <label className="label text-red-600">
             Email is already being used
           </label>
-        )}
+        )} */}
+          </div>
+        </fieldset>
 
         <button
-          className="btn btn-outline btn-secondary mt-4"
+          // className="btn btn-outline btn-secondary mt-4"
+          className="button-base mt-4"
           disabled={!formData.nickname || !formData.email || isLoading}
           onClick={() => void onSubmitClicked()}
         >
           {isLoading ? (
-            <span className="loading loading-ring loading-xl"></span>
+            <span className="loading loading-ring loading-lg"></span>
           ) : (
-            "Create Account"
+            <span className="text-lg">Create Account</span>
           )}
         </button>
 
-        <div className="divider divider-secondary">OR</div>
+        <div className="divider bg-base-100">OR</div>
         <p className="text-center">Do you already have an account?</p>
-        <Link to="/signin" className="link link-secondary text-center">
+
+        <Link to="/signin" className="link link-hover text-center font-bold">
           Log In
         </Link>
-      </fieldset>
-
-      <Toaster position="top-right" />
+      </div>
 
       {newUserData && showModal && (
         <CodeModal
--- client/src/services/mockAutService.ts
+++ client/src/services/mockAutService.ts
@@ -1,20 +1,40 @@
-import type { UserType } from "../types/user";
-import type { SignInData } from "../types/user";
-
-const mockUser: UserType = {
-  id: 2,
-  nickname: "jorgeo",
-  email: "vargklee@hotmail.com",
-  role: { id: 1, name: "Admin" },
-};
+import type { UserType } from '../types/user';
+import type { SignInData, UserSignUpType, SignUpData } from '../types/user';
 
 export const mockLogin = async (data: SignInData): Promise<UserType> => {
   // eslint-disable-next-line @typescript-eslint/typedef
   await new Promise((resolve) => setTimeout(resolve, 800));
+  const mockUser: UserType = {
+    id: 2,
+    nickname: 'jorgeo',
+    email: 'vargklee@hotmail.com',
+    role: { id: 1, name: 'Admin' },
+  };
 
-  if (data.code === "1234") {
+  if (data.code === '1234') {
     return mockUser;
   }
 
-  throw new Error("Invalid credentials.");
+  throw new Error('Invalid credentials.');
+};
+
+export const mockSignUp = async (data: SignUpData): Promise<UserSignUpType> => {
+  // eslint-disable-next-line @typescript-eslint/typedef
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const mockUserSignUnResponse: UserSignUpType = {
+    user: {
+      id: 45,
+      nickname: data.nickname,
+      email: data.email,
+      role: { id: 1, name: 'Admin' },
+    },
+    code: 'HEYJUDE5',
+    emailSent: false,
+  };
+
+  if (data.nickname !== 'failed') {
+    return mockUserSignUnResponse;
+  }
+
+  throw new Error('Invalid credentials.');
 };
